1431584d345b2b7021b7fcbe26ba3cda44b8573a
hackle.wayne <hackle.wayne@xero.com>
2021-03-16 18:35:11 +1300

    show an image

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <img alt="mi imagia" src="https://pixabay.com/get/g27bff157a2a6d5c92a2bebe3850e59356f20044e4a5d2f62a130a9dc12cc61b1b7750523deb960cad88f98b67f51584d753d5eb6c1c7d240cf4f2c24bda6f9ba_640.jpg" />
-    </div>
+    <img alt="mi imagia" src="https://pixabay.com/get/g27bff157a2a6d5c92a2bebe3850e59356f20044e4a5d2f62a130a9dc12cc61b1b7750523deb960cad88f98b67f51584d753d5eb6c1c7d240cf4f2c24bda6f9ba_640.jpg" />
   );
 }
 
