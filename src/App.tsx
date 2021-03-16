6c22e5b01481b9f6081b1b427eec6c980bba3436
hackle.wayne <hackle.wayne@xero.com>
2021-03-16 19:17:59 +1300

    inline set degreee

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,17 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  function rotate() {
-    setDegree(deg => deg + 20);
-  }
-
-  const [rotationDegree, setDegree] = useState(0);
+  const [degree, setDegree] = useState(0);
   return (
     <div>
       <img 
         alt="mi imagia" 
         style={{
-          transform: `rotate(${rotationDegree}deg)`
+          transform: `rotate(${degree}deg)`
         }}
         src="https://pixabay.com/get/g27bff157a2a6d5c92a2bebe3850e59356f20044e4a5d2f62a130a9dc12cc61b1b7750523deb960cad88f98b67f51584d753d5eb6c1c7d240cf4f2c24bda6f9ba_640.jpg" />
       <button
-        onClick={() => rotate()}>Rotate</button>
+        onClick={() => setDegree(deg => deg + 20)}>Rotate</button>
     </div>
   );
 }
