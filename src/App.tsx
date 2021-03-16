bdadafa51cdf617788a9304f190c1e2ecb05accf
hackle.wayne <hackle.wayne@xero.com>
2021-03-16 19:09:15 +1300

    rotate

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
 
 function App() {
+  const rotationDegree = 20;
   return (
-    <img alt="mi imagia" src="https://pixabay.com/get/g27bff157a2a6d5c92a2bebe3850e59356f20044e4a5d2f62a130a9dc12cc61b1b7750523deb960cad88f98b67f51584d753d5eb6c1c7d240cf4f2c24bda6f9ba_640.jpg" />
+    <img 
+      alt="mi imagia" 
+      style={{
+        transform: `rotate(${rotationDegree}deg)`
+      }}
+      src="https://pixabay.com/get/g27bff157a2a6d5c92a2bebe3850e59356f20044e4a5d2f62a130a9dc12cc61b1b7750523deb960cad88f98b67f51584d753d5eb6c1c7d240cf4f2c24bda6f9ba_640.jpg" />
   );
 }
 
